11643b109a1577cf0663f52e2423373a24b11400
HumphreyJ <joshhumphrey1@gmail.com>
2019-12-11 18:26:06 -0600

    made some changes to player styling.

--- outpost-radio/src/components/Player.jsx
+++ outpost-radio/src/components/Player.jsx
@@ -6,13 +6,21 @@ const Player = () => {
 
     const PlayerWrap = styled.div`
     width: 50%;
-    height: 15em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
     margin: 2em auto;
     background-color: black;
     h3 {
         color: white;
         width: 20%;
     }
+    audio {
+        
+    }
+    
     
     `
     return (
@@ -22,7 +30,11 @@ const Player = () => {
                 <ReactPlayer 
                 url='https:\/\/sync.outpost.radio\/radio\/8000\/radio.mp3?1576106012' 
                 playing
-                controls={true} />
+                controls={true}
+                display= 'inline-block'
+                width= '80%'
+                
+                 />
   }
                 
             </PlayerWrap>
